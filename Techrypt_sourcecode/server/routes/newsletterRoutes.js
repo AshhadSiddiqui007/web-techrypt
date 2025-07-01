e4694053a25731f56186e8c6190d14c3628b67b9
Feynman-0 <muhammad.mudassir2001@gmail.com>
2025-07-02 00:08:42 +0500

    footer changes

--- Techrypt_sourcecode/server/routes/newsletterRoutes.js
+++ Techrypt_sourcecode/server/routes/newsletterRoutes.js
@@ -6,6 +6,6 @@ const newsletterController = require('../controllers/newsletterController');
 router.post('/save-newsletter', newsletterController.saveNewsletterContent);
 router.post('/send-newsletter', newsletterController.sendNewsletter);
 router.get('/latest-newsletter', newsletterController.getLatestNewsletter);
-// router.get('/newsletter-stats', adminController.getNewsletterStats);
+ router.get('/newsletter-stats', adminController.getNewsletterStats);
 
 module.exports = router;
